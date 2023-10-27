7046711dc1f8cdce14c66de38e9f25fc44ad9d7c
Osmar Filbido <osmar.filbido@gmail.com>
2023-10-27 00:57:14 -0300

    Update the theme and color

--- app/src/main/java/com/sirfilbido/bookofstarwars/ui/theme/Color.kt
+++ app/src/main/java/com/sirfilbido/bookofstarwars/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.sirfilbido.bookofstarwars.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val DurasteelGrey = Color(0xFF64696C)
+val UnityWhite = Color(0xFFFFFFFF)
+val GalaxyBlack = Color(0xFF000000)
+val CoruscantBlue = Color(0xFF394A59)
+val ImperialRed = Color(0xFFBC1E22)
+val ValorGreen = Color(0xFF556567)
+val DroidYellow = Color(0xFFFFE81F)
--- app/src/main/java/com/sirfilbido/bookofstarwars/ui/theme/Theme.kt
+++ app/src/main/java/com/sirfilbido/bookofstarwars/ui/theme/Theme.kt
@@ -16,15 +16,16 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = DurasteelGrey,
+    secondary = CoruscantBlue,
+    tertiary = ValorGreen,
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = DurasteelGrey,
+    secondary = CoruscantBlue,
+    tertiary = ValorGreen,
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
