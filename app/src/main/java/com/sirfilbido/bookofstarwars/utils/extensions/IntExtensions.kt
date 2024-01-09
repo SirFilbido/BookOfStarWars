6633cb6464125c85e699de66e52fdf5d77aa238b
Osmar Filbido de Andrade Filho <osmar.filbido@gmail.com>
2024-01-09 01:07:40 -0300

    Add new extensions

--- app/src/main/java/com/sirfilbido/bookofstarwars/utils/extensions/IntExtensions.kt
+++ app/src/main/java/com/sirfilbido/bookofstarwars/utils/extensions/IntExtensions.kt
@@ -1,18 +1,18 @@
 package com.sirfilbido.bookofstarwars.utils.extensions
 
 fun Int.formatMass(): String {
-    return "${this}kg"
+    return if (this == 0) "Unknown"
+    else if (this > 999) "%,d".format(this).replace(',', '.').plus("kg")
+    else "${this}kg"
 }
 
 fun Int.formatHeight(): String {
     val meters = this / 100
     val centimeters = this % 100
 
-    return if (meters >= 1) {
-
-        val centimetersPart =
-            if (centimeters == 0) "00" else "$centimeters"
-
+    return if (this == 0) "Unknown"
+    else if (meters >= 1) {
+        val centimetersPart = if (centimeters == 0) "00" else "$centimeters"
         "$meters.${centimetersPart}m"
     } else {
         "${centimeters}cm"
